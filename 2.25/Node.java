fe3677185e90d44ea88c5b288c8885c83117dc69
Yvonne Li <Yvonne@YvonnedeMacBook-Air.local>
2022-02-26 01:15:25 -0800

    2.25 update

---
+++ 2.25/Node.java
@@ -0,0 +1,43 @@
+
+/**
+ *
+ * @author Rebecca
+ */
+
+public class Node<T> {
+    private T data;
+    private Node<T> next;
+
+    private Node(T dataPortion)
+    {
+        this(dataPortion, null);
+    }//end constructor
+
+    private Node(T dataPortion, Node<T> nextNode)
+    {
+        data= dataPortion;
+        next = nextNode;
+    }//end constructor
+
+    private T getData()
+    {
+        return data;
+    }//end getData
+
+    private void setData(T newData)
+    {
+        data = newData;
+    }//end setData
+
+    private Node getNextNode()
+    {
+        return next;
+    }//end getNextNode()
+
+    private void setNextNode(Node nextNode)
+    {
+        next = nextNode;
+    }//end setNextNode
+
+
+}//end Node
