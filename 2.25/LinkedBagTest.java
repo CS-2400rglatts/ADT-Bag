022947bd364a5306ae6820d3b5abbd05b2a975fd
Yvonne Li <Yvonne@YvonnedeMacBook-Air.local>
2022-02-26 01:14:51 -0800

    rewrite code LinkedBagTest

---
+++ 2.25/LinkedBagTest.java
@@ -0,0 +1,30 @@
+/*
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LinkedBagTest extends LinkedBag implements BagInterface {
+
+    @Test
+    public void testUnionDupes() {
+
+        LinkedBag<String> bag1 = new LinkedBag<String>();
+        bag1.add("1");
+        bag1.add("2");
+        bag1.add("3");
+        bag1.add("3");
+
+        LinkedBag<String> bag2 = new LinkedBag<String>();
+        bag2.add("1");
+        bag2.add("3");
+        bag2.add("5");
+        bag2.add("7");
+
+
+
+        assertEquals("1,2,3,3,1,3,5,7",LinkedBag.difference(1,2,3,3,1,3,5,7));
+    }
+}
+
+
+ */
