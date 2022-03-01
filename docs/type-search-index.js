fd8eae4d7fdb3326eed76482a3510754003a16e7
rhglatts <rhglatts@gmail.com>
2022-02-28 20:59:44 -0800

    Delete docs directory

--- docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"com.bagadt","l":"bagDriver"},{"p":"com.bagadt","l":"BagInterface"},{"p":"com.bagadt","l":"LinkedBag"},{"p":"com.bagadt","l":"LinkedBagTest"},{"p":"com.bagadt","l":"Node"},{"p":"com.bagadt","l":"ResizeableArrayBag"},{"p":"com.bagadt","l":"ResizeableArrayBagTest"}];updateSearchResults();
